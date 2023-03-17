0888a37c4dd30bed657a1c6bc7c6496cd4154109
Dreamwave7 <dima190179@gmail.com>
2023-03-17 15:26:41 +1300

    remake
    reduced python modules,
    add hard task to input
    also added dialogue window when user want close bot

--- utils.py
+++ utils.py
@@ -1,5 +1,20 @@
-from instructions import COMMANDS_DICT
+from commands import (hello_func, exit_func, add_func, change_phone_func, show_func, search_func, del_phone_func,
+                      del_func, birthday_func, next_birthday_func)
 
+COMMANDS_DICT = {
+    'hello': hello_func,
+    'exit': exit_func,
+    'close': exit_func,
+    'good bye': exit_func,
+    'add': add_func,
+    'change phone': change_phone_func,
+    'show all': show_func,
+    'phone': search_func,
+    'delete phone': del_phone_func,
+    'delete': del_func,
+    'birthday': birthday_func,
+    'days to birthday': next_birthday_func
+}
 
 def change_input(user_input):
     new_input = user_input
