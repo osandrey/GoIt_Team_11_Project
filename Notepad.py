06a0b75908cfdaa813145c7966fa18bea16317b3
Inna H <strat.ru.ge@gmail.com>
2023-03-20 19:48:29 +0000

    updated

--- Notepad.py
+++ Notepad.py
@@ -4,6 +4,11 @@ import pickle
 import re
 from collections import UserDict
 from typing import Callable
+from colorit import *
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+
+colorit.init_colorit()
 
 class MyException(Exception):
     pass
@@ -12,20 +17,20 @@ class Notepad(UserDict):
 
     def __getitem__(self, title):
         if not title in self.data.keys():
-            raise MyException("This article isn't in the Notepad")
+            raise MyException(color("This article isn't in the Notepad",Colors.red))
         note = self.data[title]
         return note
     
     def add_note(self, note) -> str:
         self.data.update({note.title.value:note})
-        return 'Done!'
+        return color('Done!',Colors.blue)
 
     def delete_note(self, title):
         try:
             self.data.pop(title)
-            return f"{title} was removed"
+            return color(f"{title} was removed",Colors.purple)
         except KeyError:
-            return "This note isn't in the Notepad"
+            return color("This note isn't in the Notepad",Colors.blue)
 
     def get_notes(self, file_name):
         with open(file_name, 'ab+') as fh:
@@ -39,7 +44,8 @@ class Notepad(UserDict):
         pass
 
     def show_notes_titles(self):
-        return "\n".join([note for note in notes])
+        res =  "\n".join([note for note in notes])
+        return color(res,Colors.orange)
     
     def write_notes(self, file_name):
         with open(file_name, "wb") as fh:
@@ -63,7 +69,7 @@ class NoteTitle(Field):
     @value.setter
     def value(self, title):
         if len(title) == 0:
-            raise ValueError("The title wasn't added. It should have at least 1 character.")
+            raise ValueError(color("The title wasn't added. It should have at least 1 character.",Colors.red))
         self.__value = title
 
 class NoteBody(Field):
@@ -86,13 +92,12 @@ class Note:
         self.body = body
 
     def show_note(self):
-        return '\n'.join([self.title.value, self.body.value, ', '.join([f"#{tag.value}" for tag in self.tags])])
+        return '\n'.join([f"Title: {self.title.value}", f"Body: {self.body.value}", f"Tags: {self.show_tags()}"])
     
     def show_tags(self):
         if self.tags == []:
-            return ""
-        return ", ".join([tag.value for tag in self.tags])
-                      
+            return "Tags: Empty",Colors.red
+        return ', '.join([tag.value for tag in self.tags])
         
 def decorator_input(func: Callable) -> Callable:
     @functools.wraps(func)
@@ -102,20 +107,20 @@ def decorator_input(func: Callable) -> Callable:
         except KeyError as err:
             return err
         except IndexError:
-            return "You didn't enter the title or keywords"
+            return color("You didn't enter the title or keywords",Colors.red)
         except TypeError:
-            return "Sorry, this command doesn't exist"
+            return color("Sorry, this command doesn't exist",Colors.red)
         except Exception as err:
             return err
     return wrapper
 
 @decorator_input
 def add_note(*args) -> str:
-    title = NoteTitle(input("Enter the title: "))
+    title = NoteTitle(input(color("Enter the title: ",Colors.yellow)))
     if title.value in notes.data.keys():
-        raise MyException('This title already exists')
-    body = NoteBody(input("Enter the note: "))
-    tags = input("Enter tags (separate them with ',') or press Enter to skip this step: ")
+        raise MyException(color('This title already exists',Colors.red))
+    body = NoteBody(input(color("Enter the note: ",Colors.yellow)))
+    tags = input(color("Enter tags (separate them with ',') or press Enter to skip this step: ",Colors.yellow))
     tags = [NoteTag(t.strip()) for t in tags.split(',')]
     note = Note(title, body, tags)
     return notes.add_note(note)
@@ -130,14 +135,13 @@ def edit_note(*args) -> str:
     title = args[0]
     if title in notes.data.keys():
         note = notes.data.get(title)
-    user_title = input("Enter new title or press 'enter' to skip this step: ")
+    user_title = input(color("Enter new title or press 'enter' to skip this step: ",Colors.yellow))
     if user_title:
         if not user_title in notes.data.keys():
             notes.data[user_title] = notes.data.pop(title)
             note.edit_title(NoteTitle(user_title))
         else:
-            raise MyException('This title already exists.')
-    
+            raise MyException(color('This title already exists.',Colors.red))
     try:
         body = edit(note.body.value, 'body')
         if body:
@@ -167,25 +171,24 @@ def show_note(*args:str) -> str:
 
 @decorator_input
 def edit(text: str, part) -> str:
-    user_input = input(f"Enter any letter if you want to edit {part} or press 'enter' to skip this step. ")
+    user_input = input(color(f"Enter any letter if you want to edit {part} or press 'enter' to skip this step. ",Colors.green))
     if user_input:
         with open('edit_note.txt', 'w') as fh:
             fh.write(text)
         subprocess.Popen(['open', 'edit_note.txt'])
         mes = ''
         if part == 'tags':
-            mes = "Separate tags with ','"
-        input(f'Press enter or any letter if you finished editing. Please, make sure you closed the text editor. {mes}')
+            mes = color("Separate tags with ','",Colors.green)
+        input(color(f'Press enter or any letter if you finished editing. Please, make sure you closed the text editor. {mes}',Colors.green))
         with open('edit_note.txt', 'r') as fh:
             edited_text = fh.read()
         return edited_text
     
 def find(*args) -> str:
-
     try:
         re.match(r'^\s*$', args)
     except TypeError:
-        args = input("Enter the phrase you want to find: ")
+        args = input(color("Enter the phrase you want to find: ",Colors.yellow))
     notes_list = []
     for note in notes.data.values():
         if re.search(args, note.body.value) or re.search(args, note.title.value, flags=re.IGNORECASE):
@@ -207,16 +210,18 @@ def find_tags(*args: str) -> str:
     sorted_dict = sorted(notes_dict, key=lambda k: len(notes_dict[k]), reverse=True)
     return '\n'.join([f"{key}:{notes_dict[key]}" for key in sorted_dict if len(notes_dict[key]) > 0])
 
-
 def get_command(words: str) -> Callable:
+    
+    if words[0] == '':
+        raise KeyError ("This command doesn't exist")
     for key in commands_dict.keys():
         try:
             if re.search(fr'\b{words[0].lower()}\b', str(key)):
                 func = commands_dict[key]
                 return func
-        except re.error:
+        except (re.error):
             break
-    raise KeyError("This command doesn't exist") 
+    raise KeyError ("This command doesn't exist")
 
 notes = Notepad()
 notes.get_notes('notes.bin')
@@ -230,12 +235,12 @@ commands_dict = {('add', 'add_note'):add_note,
                  ('delete',):delete_note,
                  ('goodbye','close','exit','quit'):goodbye
 }
-    
 
+word_completer = WordCompleter(["add", "add_note", "edit", "edit_note", "show", "show_note", "showall" ,"find_tags", "find", "delete" ,"."])
 def main():
 
     while True:
-        words = input(">>> ").split(' ')
+        words = prompt('Enter your command: ', completer=word_completer).split(" ")
         try:
             func = get_command(words)
         except KeyError as error:
